d29333d13bf2c0c27303f391a53115ea2458d23e
Peitra Erdi <peitrae@gmail.com>
2022-03-28 15:36:21 +0700

    Add small size attribute

--- components/typhographies/Title/utils.types.ts
+++ components/typhographies/Title/utils.types.ts
@@ -2,6 +2,7 @@ export interface FontSizeByKeys {
 	xl: 'ExtraLarge';
 	lg: 'Large';
 	md: 'Medium';
+  sm: 'Small';
 	xs: 'ExtraSmall';
 }
 
@@ -9,6 +10,7 @@ export interface FontSizeByTags {
 	h1: 'ExtraLarge';
 	h2: 'Large';
 	h3: 'Medium';
+  h4: 'Small';
 	h5: 'ExtraSmall';
 }
 
