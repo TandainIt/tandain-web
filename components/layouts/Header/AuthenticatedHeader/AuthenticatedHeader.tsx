8e5db352b32b67285160f86a3d1e3bac3a45bb6e
Peitra Erdi <peitrae@gmail.com>
2022-04-13 09:35:18 +0700

    Add image to avatar

--- components/layouts/Header/AuthenticatedHeader/AuthenticatedHeader.tsx
+++ components/layouts/Header/AuthenticatedHeader/AuthenticatedHeader.tsx
@@ -1,4 +1,6 @@
 import { FC, useRef, useState } from 'react';
+import Image from 'next/image';
+import clsx from 'clsx';
 
 import useAppDispatch from '@/hooks/useAppDispatch';
 import useOnClickOutside from '@/hooks/useOnClickOutside';
@@ -51,11 +53,20 @@ const AuthenticatedHeader: FC = () => {
 			<Button
 				ref={avatarRef}
 				data-testid='avatar'
-				startIcon={<UserIcon />}
-				className={classes.Avatar}
+				// startIcon={<UserIcon />}
+				className={clsx(classes.Avatar, 'p0')}
 				onClick={toggleShowAvatarMenu}
 				round
-			></Button>
+			>
+				<Image
+					src='/temp/my-list-item-img.png'
+					alt='Profile'
+					className={classes.AvatarImg}
+					width={40}
+					height={40}
+					layout='intrinsic'
+				/>
+			</Button>
 			{showAvatarMenu && (
 				<AvatarMenu ref={avatarMenuRef} className={classes.AvatarMenu} />
 			)}
