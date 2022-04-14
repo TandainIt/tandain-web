25c1975cb19b2b0ab1c1977a1b38e6d9f326ccaa
Peitra Erdi <peitrae@gmail.com>
2022-04-14 14:07:41 +0700

    Add module federation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const { withFederatedSidecar } = require('@module-federation/nextjs-mf');
+const { dependencies } = require('./package.json');
+
+module.exports = withFederatedSidecar({
+	name: 'tandainClient', // NOTE: Don't use kebab-case
+	filename: 'static/chunks/remoteEntry.js',
+	exposes: {
+		'./Button': './components/ui/Button/Button.tsx',
+	},
+	shared: {
+		...dependencies,
+    clsx: {
+      singleton: true,
+      requiredVersion: dependencies.clsx,
+    },
+		react: {
+			singleton: true,
+			eager: true,
+			requiredVersion: dependencies.react,
+		},
+		'react-dom': {
+			singleton: true,
+			eager: true,
+			requiredVersion: dependencies['react-dom'],
+		},
+	},
+})({
+	// your original next.config.js export
+	reactStrictMode: true,
+	webpack(config) {
+    config.output.publicPath = 'http://localhost:3000/_next/'
+
+		return config;
+	},
+});
