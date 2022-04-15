115a475fa5ba509ddc3add9a96d3eed4379b3938
Peitra Erdi <peitrae@gmail.com>
2022-04-15 16:40:10 +0700

    Add small size attribute option

--- components/ui/Button/Button.types.ts
+++ components/ui/Button/Button.types.ts
@@ -5,7 +5,7 @@ import { BaseButtonProps } from '../BaseButton';
 export interface ButtonProps extends BaseButtonProps {
 	variant?: 'solid' | 'outlined' | 'text';
 	color?: 'primary' | 'dark';
-	size?: 'md' | 'lg';
+	size?: 'sm' | 'md' | 'lg';
   iconClassName?: string;
 	startIcon?: ReactNode;
   round?: Boolean 
