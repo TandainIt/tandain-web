955097c2275ecbe61e1028cc59676da286b2aa14
Peitra Erdi <peitrae@gmail.com>
2022-05-20 06:11:40 +0700

    Add no to cache control

--- next.config.js
+++ next.config.js
@@ -7,19 +7,19 @@ module.exports = withFederatedSidecar({
 	name: 'tandainClient', // NOTE: Don't use kebab-case
 	filename: 'static/chunks/remoteEntry.js',
 	exposes: {
-    './TrashIcon': './components/icons/TrashIcon.tsx',
-    './CheckmarkIcon': './components/icons/CheckmarkIcon.tsx',
+		'./TrashIcon': './components/icons/TrashIcon.tsx',
+		'./CheckmarkIcon': './components/icons/CheckmarkIcon.tsx',
 		'./Button': './components/ui/Button/index.ts',
-    './Spinner': './components/ui/Spinner/index.ts',
-    './Tooltip': './components/ui/Tooltip/index.ts',
-    './Title': './components/typhographies/Title/index.ts'
+		'./Spinner': './components/ui/Spinner/index.ts',
+		'./Tooltip': './components/ui/Tooltip/index.ts',
+		'./Title': './components/typhographies/Title/index.ts',
 	},
 	shared: {
 		...dependencies,
-    clsx: {
-      singleton: true,
-      requiredVersion: dependencies.clsx,
-    },
+		clsx: {
+			singleton: true,
+			requiredVersion: dependencies.clsx,
+		},
 		react: {
 			singleton: true,
 			eager: true,
@@ -33,9 +33,23 @@ module.exports = withFederatedSidecar({
 	},
 })({
 	// your original next.config.js export
+  async headers() {
+    return [
+      {
+        source: '*',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, max-age=0, must-revalidate',
+          }
+        ],
+      },
+    ]
+  },
 	reactStrictMode: true,
 	webpack(config) {
-    config.output.publicPath = 'auto'
+		config.output.publicPath = 'auto';
 
 		return config;
 	},
