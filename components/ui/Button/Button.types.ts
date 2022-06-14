1534dd61aee4317c49f3e0b5b0b90863eefbf1b6
Peitra Erdi <peitrae@gmail.com>
2022-06-15 00:14:08 +0700

    Add xs size

--- components/ui/Button/Button.types.ts
+++ components/ui/Button/Button.types.ts
@@ -5,7 +5,7 @@ import { BaseButtonProps } from '../BaseButton';
 export interface ButtonProps extends BaseButtonProps {
 	variant?: 'solid' | 'outlined' | 'text';
 	color?: 'primary' | 'dark';
-	size?: 'sm' | 'md' | 'lg';
+	size?: 'xs' | 'sm' | 'md' | 'lg';
   iconClassName?: string;
 	startIcon?: ReactNode;
   round?: Boolean 
