32fca32a32d1276f9f20bd7b6845b338eb72c2b4
Peitra Erdi <peitrae@gmail.com>
2022-08-22 05:42:30 +0700

    Restructuring icons

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,6 +1,5 @@
 export { default as TrashLightIcon } from './TrashLightIcon';
 export { default as GoogleIcon } from './GoogleIcon';
-export { default as CheckmarkIcon } from './CheckmarkIcon';
 export { default as ListIcon } from './ListIcon';
 export { default as MenuIcon } from './MenuIcon';
 export { default as UserIcon } from './UserIcon';
