7fec3478e9b907bdaaeb360dbcb3335ad7510f89
Peitra Erdi <peitrae@gmail.com>
2022-08-29 23:19:50 +0700

    Show spinner when it is waiting for window.opener

--- pages/auth/google-oauth.tsx
+++ pages/auth/google-oauth.tsx
@@ -1,4 +1,7 @@
+import { Spinner } from '@/components/ui';
 import { sendAuthCodeToWindowParent } from '@/modules/auth/utils';
+
+import classes from '@/modules/auth/GoogleOauthLoadingPage/GoogleOauthLoadingPage.module.sass';
 
 const GoogleOauthLoading = () => {
 	if (typeof window !== 'undefined' && window.opener) {
@@ -6,7 +9,11 @@ const GoogleOauthLoading = () => {
 		window.close();
 	}
 
-	return <div>Loading...</div>;
+	return (
+		<main className={classes.Main}>
+			<Spinner />
+		</main>
+	);
 };
 
 export default GoogleOauthLoading;
