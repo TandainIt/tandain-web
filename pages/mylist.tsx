3e56ca5045a41dc2be386ce3dc325d28e9b9b2b7
Peitra Erdi <peitrae@gmail.com>
2022-09-09 01:28:13 +0700

    TAN-81 Add Loading and Article List is Empty State

--- pages/mylist.tsx
+++ pages/mylist.tsx
@@ -1,14 +1,17 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
 import clsx from 'clsx';
 
 import { AuthenticatedHeader, Page, Sidebar } from '@/components/layouts';
 import { Title } from '@/components/typhographies';
+import { Button, Spinner } from '@/components/ui';
 import { ArticleListItem } from '@/modules/article';
 
 import classes from '@/modules/article/MyListPage/MyListPage.module.sass';
+import EmptyListSVG from '@/public/illustration/empty-list.svg';
 
-const MyListPage: NextPage = () => {
+const useMyListPage = () => {
 	const myList = [
 		{
 			id: 'title-1',
@@ -68,28 +71,57 @@ const MyListPage: NextPage = () => {
 		},
 	];
 
+	return { isLoading: true, list: [] };
+};
+
+const EmptyList = () => (
+	<div className={classes.EmptyList}>
+		<Image src={EmptyListSVG} alt='List is empty' />
+		<div>
+			<Title as='h3' className={classes.EmptyListTitle}>
+				Your list is empty
+			</Title>
+			<p className={classes.EmptyListParagraph}>
+				Looks like you haven’t saved anything to your list. Start to{' '}
+				<Button variant='base'>add inspiration.</Button>
+			</p>
+		</div>
+	</div>
+);
+
+const MyListPage: NextPage = () => {
+	const { isLoading, list } = useMyListPage();
+
 	return (
 		<>
 			<Head>
 				<title>My List</title>
 			</Head>
-			<Page className='col'>
+			<Page className='inline-flex'>
 				<Sidebar />
-				<div className='w-full'>
+				<div className='w-full flex flex-col'>
 					<AuthenticatedHeader />
-					<ol data-testid='list' className={clsx(classes.List, 'mt4 mb5')}>
-						<Title className={clsx(classes.Title)}>My List</Title>
-						{myList.map((item) => (
-							<ArticleListItem
-								key={item.id}
-								title={item.title}
-								sourceName={item.sourceName}
-								sourceURL={item.sourceURL}
-								imgURL={item.imgURL}
-								onDelete={() => {}}
-							/>
-						))}
-					</ol>
+					<main>
+						<ol data-testid='list' className={clsx(classes.List, 'mt4 mb5')}>
+							<Title className={classes.Title}>My List</Title>
+							{isLoading ? (
+								<Spinner className='absolute-centered' />
+							) : !list.length ? (
+								<EmptyList />
+							) : (
+								list.map((item) => (
+									<ArticleListItem
+										key={item.id}
+										title={item.title}
+										sourceName={item.sourceName}
+										sourceURL={item.sourceURL}
+										imgURL={item.imgURL}
+										onDelete={() => {}}
+									/>
+								))
+							)}
+						</ol>
+					</main>
 				</div>
 			</Page>
 		</>
