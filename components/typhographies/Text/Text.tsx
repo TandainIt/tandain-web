fcaccb35609500c5ea5da1d3bdd6449f2cab35bb
Peitra Erdi <peitrae@gmail.com>
2022-10-05 09:36:25 +0700

    Add danger color variant

--- components/typhographies/Text/Text.tsx
+++ components/typhographies/Text/Text.tsx
@@ -4,29 +4,25 @@ import { createElement, FC } from 'react';
 import classes from './Text.module.sass';
 
 import { TextProps } from './Text.types';
-import { fontTypes, sizes as fontSize } from '@/utils/variables';
+import { fontTypes, sizes as fontSize, colors } from '@/utils/variables';
 
 const Text: FC<TextProps> = ({
 	as = 'span',
 	font = 'sans-serif',
-  size = 'md',
+	size = 'md',
+	color,
 	className,
 	...args
 }) => {
-
 	return createElement(as, {
-		className: clsx(classes[fontTypes[font]], classes[fontSize[size]], className),
+		className: clsx(
+			classes[fontTypes[font]],
+			classes[fontSize[size]],
+			classes[colors[color]],
+			className
+		),
 		...args,
 	});
-
-	// return (
-	// 	<span
-	// 		className={clsx(classes.Text, labelVariants[variant], className)}
-	// 		{...args}
-	// 	>
-	// 		{children}
-	// 	</span>
-	// );
 };
 
 export default Text;
